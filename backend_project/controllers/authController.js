9522ab2285e901dc220ec6decbafab621de277a4
Sakshi <sakshiprofessional098@gmail.com>
2026-02-27 18:59:26 +0530

    errors resolved

--- backend_project/controllers/authController.js
+++ backend_project/controllers/authController.js
@@ -14,6 +14,7 @@ export const registerUser = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
+    console.error("Register error:", error);
     res.status(500).json({ message: "Server error" });
   }
 };
--- backend_project/server.js
+++ backend_project/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
 import dotenv from 'dotenv';
 
-const app = express();
 dotenv.config();
+
+const app = express();
+if (process.env.NODE_ENV === "production") {
+  app.use(cors({
+    origin: "https://mern-test-sakshi-malik565.vercel.app",
+    credentials: true
+  }));
+} else {
+  app.use(cors());
+}
+app.use(express.json());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
