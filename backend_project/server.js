8abd836baac2c46235be716060f2fe3248f22b1a
Sakshi <sakshiprofessional098@gmail.com>
2026-02-27 14:42:21 +0530

    implemented APIs

---
+++ backend_project/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import authRoutes from './routes/authRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('MongoDB connected'))
+        .catch(err => console.log(err));
+});
